00724ef1bc99dfe129c07603006affb8888ca77c
felipedonato280 <felipedonato280@gmail.com>
2023-03-20 11:04:44 -0300

    btnDetalhesnoConsole

--- script/script.js
+++ script/script.js
@@ -1,47 +1,17 @@
 let inputBuscarFilme = document.querySelector("#input-buscar-filme")
 let btnBuscarFilme = document.querySelector("#btn-buscar-filme")
 
-btnBuscarFilme.onclick = () => {
-    if(inputBuscarFilme.value.length > 0){
-        let filmes =  new Array()
-        fetch("http://www.omdbapi.com/?apikey=21779dab&s="+inputBuscarFilme.value)
-        .then((resp) => resp.json())
-        .then((resp)=> {
-            resp.Search.forEach((item) => {
-                console.log(item)
-                let filme=new Filme(
-                    item.imdbId,
-                    item.Title,
-                    item.Year,
-                    null,
-                    null,
-                    item.Poster,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null
-                )
-                filmes.push(filme)
-            });
-            listarFilmes(filmes)
-        })
-    }
-    return false;
-}
-
 let listarFilmes = async (filmes) => {
     let listaFilmes = await document.querySelector("#lista-filmes");
     listaFilmes.innerHTML = "";
-    console.log(listaFilmes)
+    // console.log(listaFilmes);
     if(filmes.length > 0){
         filmes.forEach(async(filme) => {
             console.log(filme);
-            listaFilmes.appendChild(await filme.getCard())
+            listaFilmes.appendChild(await filme.getCard());
             filme.getBtnDetalhes().onclick=()=>{
-                detalhesFilme(filme.id)
+                detalhesFilme(filme.id);
             }
-
         });
     }
 }
@@ -52,9 +22,10 @@ let detalhesFilme = async (id) =>{
     .then((resp) => {
 
         console.log(resp);
-        let filme=new Filme(
+        let filme= new Filme(
             resp.imdbID,
             resp.Title,
+            resp.Year,
             resp.Genre.split(","),
             resp.Runtime,
             resp.Poster,
@@ -63,9 +34,38 @@ let detalhesFilme = async (id) =>{
             resp.Actors.split(","),
             resp.Awards,
             resp.imdbRating
-        ) 
+        )
 
         console.log(filme);
 
     });
 }
+
+btnBuscarFilme.onclick = () => {
+    if(inputBuscarFilme.value.length > 0){
+        let filmes =  new Array();
+        fetch("http://www.omdbapi.com/?apikey=21779dab&s="+inputBuscarFilme.value)
+        .then((resp) => resp.json())
+        .then((resp)=> {
+                resp.Search.forEach((item) => {
+                    console.log(item);
+                    let filme=new Filme(
+                        item.imdbID,
+                        item.Title,
+                        item.Year,
+                        null,
+                        null,
+                        item.Poster,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+                    );
+                    filmes.push(filme);
+                });
+                listarFilmes(filmes);
+        });
+    }
+    return false;
+};
